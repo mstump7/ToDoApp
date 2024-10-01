2436f6a60b4d3edb08eca71393ae6abeb5012cae
Madison Stump <180679597+mstump7@users.noreply.github.com>
2024-10-01 20:32:22 +0000

    commit part3

--- main.java
+++ main.java
@@ -1,19 +1,17 @@
-import java.util.Date;
-
 public class TodoList {
     // Class to represent a single Todo item with a description and a due date
     class TodoItem {
         String description;
-        Date dueDate;
+        String dueDate;  // Added from add-date branch
 
-        public TodoItem(String description, Date dueDate) {
+        public TodoItem(String description, String dueDate) {
             this.description = description;
-            this.dueDate = dueDate;
+            this.dueDate = dueDate; // Initialize due date
         }
 
         @Override
         public String toString() {
-            return "Task: " + description + " | Due Date: " + dueDate;
+            return "Task: " + description + ", Due: " + dueDate; // Modified to include due date
         }
     }
 
@@ -27,16 +25,16 @@ public class TodoList {
         count = 0;
     }
 
-    // Method to add a Todo item
-    public void addTodoItem(String description) {
+    // Method to add a new Todo item (From add-todo branch)
+    public void addTodoItem(String description, String dueDate) {
         if (count < todoItems.length) {
-            todoItems[count] = new TodoItem(description);
+            todoItems[count] = new TodoItem(description, dueDate); // Use description and due date
             count++;
         } else {
             System.out.println("Todo list is full!");
         }
     }
-    
+
     // Method to display all Todo items
     public void displayTodoItems() {
         for (int i = 0; i < count; i++) {
@@ -48,10 +46,10 @@ public class TodoList {
         // Create a TodoList object with space for 5 items
         TodoList myTodoList = new TodoList(5);
 
-        // Add some todo items
-        myTodoList.addTodoItem("Finish Java assignment");
-        myTodoList.addTodoItem("Grocery shopping");
-        
+        // Add a few Todo items
+        myTodoList.addTodoItem("Finish project", "2024-10-05");
+        myTodoList.addTodoItem("Buy groceries", "2024-10-06");
+
         // Display all items
         myTodoList.displayTodoItems();
     }
